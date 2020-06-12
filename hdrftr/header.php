bf2440c4cf557646db6c92f79418b2cfe29fbbbe
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-06-12 04:29:39 -0400

    added front page and basic images

---
+++ hdrftr/header.php
@@ -0,0 +1,55 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/slideshow.css">
+    <link rel="stylesheet" href="assets/css/heading.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/sign_up.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+
+    <title>NAVIGATIONn</title>
+</head>
+
+<body>
+<nav>
+    <div class="logo">
+        <img class="logoimage" src="assets/icons/Logo.png" alt="">
+        <h4>SOCIAL SIGN</h4>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="index.php">Home</a
+            ></li>
+        <li>
+            <a href="user_petitions.php">Petitions</a>
+        </li>
+        <li class="id1">
+            <a href="user_requests.php">Request</a>
+        </li>
+        <li id="id2">
+            <a href="login.php">login</a>
+        </li>
+        <li id="id2">
+            <a href="signup.php">register</a>
+        </li>
+        <li id="id2">
+            <a href="logout.php">logout</a>
+        </li>
+    </ul>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+
+    </div>
+</nav>
+
+
+
